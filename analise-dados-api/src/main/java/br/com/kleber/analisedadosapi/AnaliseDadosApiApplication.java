207e8fcfc2f99f17b29b9772f504b6ad3e412fd1
Kleber Rezende <kleber.rzd@gmail.com>
2020-10-01 20:42:13 -0300

    Organizado arquivos.

--- analise-dados-api/src/main/java/br/com/kleber/analisedadosapi/AnaliseDadosApiApplication.java
+++ analise-dados-api/src/main/java/br/com/kleber/analisedadosapi/AnaliseDadosApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnaliseDadosApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnaliseDadosApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnaliseDadosApiApplication.class, args);
+    }
 
 }
