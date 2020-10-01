306c473a743c0694db7674862e13461e3689ff9a
Kleber Rezende <kleber.rzd@gmail.com>
2020-10-01 20:32:18 -0300

    add projeto

---
+++ analise-dados-api/src/main/java/br/com/kleber/analisedadosapi/AnaliseDadosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kleber.analisedadosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDadosApiApplication.class, args);
+	}
+
+}
